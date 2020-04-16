3b204ae5f9690ca2f6bc359824e877aee136f77a
Alexa Kelly <59925735+kellyav@users.noreply.github.com>
2020-04-15 21:11:05 -0400

    Create hello_world.java

---
+++ hello_world.java
@@ -0,0 +1,10 @@
+package examples;
+  public class hello_world {
+    public static void message(String myString, int myInt) {
+      System.out.println("This is my String: " +myString);
+      System.out.println("and this is my int: " + myInt);
+      }
+    public static void main(String[] args) {
+      message("Hello World!", 4);
+    }
+}
